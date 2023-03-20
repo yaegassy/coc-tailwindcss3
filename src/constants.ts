489e98d944956d9516d69abecd2007ba15914fbd
yaegassy <yaegassy@users.noreply.github.com>
2023-03-20 21:30:04 +0900

    feat: support TS/ESM config files
    REF: https://github.com/tailwindlabs/tailwindcss-intellisense/pull/738/commits/073df128b23dbe335be0c93d5dbde9793391351b

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-// PORTING: https://github.com/tailwindlabs/tailwindcss-intellisense/blob/55d2b9e8da44faa55971ef006c0750fed5f0da2d/packages/tailwindcss-language-server/src/lib/constants.ts
+// PORTING: https://github.com/tailwindlabs/tailwindcss-intellisense/blob/master/packages/tailwindcss-language-server/src/lib/constants.ts
 
-export const CONFIG_GLOB = '{tailwind,tailwind.config,tailwind.*.config,tailwind.config.*}.{js,cjs}';
+export const CONFIG_GLOB = '{tailwind,tailwind.config,tailwind.*.config,tailwind.config.*}.{js,cjs,ts,mjs}';
 export const PACKAGE_LOCK_GLOB = '{package-lock.json,yarn.lock,pnpm-lock.yaml}';
 export const CSS_GLOB = '*.{css,scss,sass,less,pcss}';
